49a4770de7e30a679723f159ed412d174e88aa0d
Gregory Haskins <greg@greghaskins.com>
2014-12-19 17:23:57 -0500

    change from XCUnit to Quick/Nimble

---
+++ PhotoCopierTests/PhotoCopierSpec.swift
@@ -0,0 +1,15 @@
+import Quick
+import Nimble
+
+class PhotoCopierSpec: QuickSpec {
+    override func spec() {
+        
+        describe("something") {
+            it("happens") {
+                expect(1).to(equal(1))
+            }
+        }
+
+    }
+}
+
