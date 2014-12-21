74b097ce84e9a73bedb9ec0b5df274fcdd53fe3f
Gregory Haskins <greg@greghaskins.com>
2014-12-21 07:47:28 -0500

    spike implementation of copying most recent photo
    Does a lot of things wrong, like not checking for permission or whether
    there even are any photos.

--- PhotoCopier/ViewController.swift
+++ PhotoCopier/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func copyButtonTapped(sender: UIButton) {
-        println("you tapped the button.")
+        
+        // Spike implementation, copying the most recent photo
+        
+        // TODO this doesn't check for permission first
+        // and thus crashes the first time it's run
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
+        let asset: PHAsset = fetchResult.lastObject as PHAsset
+        
+        let manager = PHImageManager.defaultManager()
+        manager.requestImageDataForAsset(asset, options: PHImageRequestOptions()) {
+            (imageData: NSData!, dataUTI: String!, orientation: UIImageOrientation, info: [NSObject : AnyObject]!) in
+            
+            PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                let image = UIImage(data: imageData)
+                let creationRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
+                
+                // TODO metadata like date, location are not yet copied
+                
+                }, completionHandler: { (success: Bool, error: NSError!) in
+                    println("success = \(success)")
+            })
+            
+        }
+        
     }
 
 
