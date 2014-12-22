c72bc6e162f55b9e18464908059153f5f5bdf151
Gregory Haskins <greg@greghaskins.com>
2014-12-22 09:07:24 -0500

    request photos permission before copying
    more spike implementation fun

--- PhotoCopier/ViewController.swift
+++ PhotoCopier/ViewController.swift
@@ -17,29 +17,32 @@ class ViewController: UIViewController {
         
         // Spike implementation, copying the most recent photo
         
-        // TODO this doesn't check for permission first
-        // and thus crashes the first time it's run
-        
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
-        let asset: PHAsset = fetchResult.lastObject as PHAsset
-        
-        let manager = PHImageManager.defaultManager()
-        manager.requestImageDataForAsset(asset, options: PHImageRequestOptions()) {
-            (imageData: NSData!, dataUTI: String!, orientation: UIImageOrientation, info: [NSObject : AnyObject]!) in
+        PHPhotoLibrary.requestAuthorization({status in
             
-            PHPhotoLibrary.sharedPhotoLibrary().performChanges({
-                let image = UIImage(data: imageData)
-                let creationRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
+            if status == PHAuthorizationStatus.Authorized {
+        
+                let fetchOptions = PHFetchOptions()
+                fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+                let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
+                let asset: PHAsset = fetchResult.lastObject as PHAsset
                 
-                // TODO metadata like date, location are not yet copied
-                
-                }, completionHandler: { (success: Bool, error: NSError!) in
-                    println("success = \(success)")
-            })
-            
-        }
+                let manager = PHImageManager.defaultManager()
+                manager.requestImageDataForAsset(asset, options: PHImageRequestOptions()) {
+                    (imageData: NSData!, dataUTI: String!, orientation: UIImageOrientation, info: [NSObject : AnyObject]!) in
+                    
+                    PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                        let image = UIImage(data: imageData)
+                        let creationRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
+                        
+                        // TODO metadata like date, location are not yet copied
+                        
+                        }, completionHandler: { (success: Bool, error: NSError!) in
+                            println("success = \(success)")
+                    })
+                    
+                }
+            }
+        });
         
     }
 
