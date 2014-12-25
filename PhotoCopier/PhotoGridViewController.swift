166fd78c5de5e2fee8aa04b0a1ced5e6afb7d5cf
Gregory Haskins <greg@greghaskins.com>
2014-12-24 22:33:09 -0500

    create custom collection view controller
    for photos grid

---
+++ PhotoCopier/PhotoGridViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class PhotoGridViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("loaded photo grid view controller")
+    }
+
+}
