fc969ddc04014776269d499ee823f05416a92f42
Gregory Haskins <greg@greghaskins.com>
2014-12-26 23:55:37 -0500

    add beginnings of collection view data source

--- PhotoCopier/PhotoGridViewController.swift
+++ PhotoCopier/PhotoGridViewController.swift
@@ -8,3 +8,20 @@ class PhotoGridViewController: UICollectionViewController {
     }
 
 }
+
+extension PhotoGridViewController: UICollectionViewDataSource {
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ThumbnailCell", forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+}
